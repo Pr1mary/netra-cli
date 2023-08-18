cbc1b9d15aada1b585da7fd0f94540859aa7df2f
Rafli <raflisatriandi@outlook.co.id>
2023-08-19 02:43:33 +0700

    add basic available port listing for testing

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,24 @@
+mod config_helper;
+mod serial_helper;
+
+use config_helper::Config;
+use serial_helper::Serial;
+
 fn main() {
-    println!("Hello, world!");
+    let serial = Serial::new();
+    // let mut config = Config::default();
+
+    // config.init_config().expect("config init fail");
+    
+    // let client_port = config.get_port();
+    // let client_baud = config.get_baud();
+
+    let ports = serial.get_avail_port_name();
+
+    println!("Avail port list:");
+    for p in ports.to_owned() {
+        println!("{}", p);
+    }
+
+
 }
